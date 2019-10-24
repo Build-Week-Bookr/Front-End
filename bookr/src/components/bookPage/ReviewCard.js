613f644a582858c472c1bdf4ac95d8f7ad38101d
CarnunMP <carnun@hotmail.co.uk>
2019-10-24 21:08:27 +0100

    new book form styled

--- bookr/src/components/bookPage/ReviewCard.js
+++ bookr/src/components/bookPage/ReviewCard.js
@@ -22,7 +22,6 @@ export function ReviewCard(props) {
             fetchUser(userId);
         }
     }, []);
-    debugger
 
     return (
         <StyledReviewCard>
--- bookr/src/components/homePage/NewBookForm.js
+++ bookr/src/components/homePage/NewBookForm.js
@@ -6,10 +6,69 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
 
 const StyledNewBookForm = styled.div`
+    background: #b3b3b3;
+    padding-top: 2rem;
+    
+    form {
+        border: 2px solid #05182a;
+        border-radius: 10px;
+        width: 30rem;
+        height: 22.5rem;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        background: #F0F0F0;
 
+        .field {
+            margin-bottom: 0.5rem;
+
+            input {
+                width: 90%;
+                height: 1.5rem;
+                text-align: center;
+                border-radius: 2px;
+                background: #b3b3b3;
+                font-family: 'Didact Gothic', sans-serif;
+                color: #05182a;
+                border: 2px solid #c8c8c8;
+            }
+
+            textarea {
+                width: 90%;
+                border-radius: 2px;
+                background: #b3b3b3;
+                resize: none;
+                border: 2px solid #c8c8c8;
+            }
+
+            h6 {
+                font-size: 0.8rem;
+            }
+        }
+
+        .submit-button {
+            flex-grow: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+            align-items: center;
+
+            button {
+                width: 6rem;
+                height: 2rem;
+                border-radius: 2px;
+                border: 2px solid #c8c8c8;
+                font-family: 'Didact Gothic', sans-serif;
+                color: #05182a;
+                font-weight: bold;
+                cursor: pointer;
+            }
+        }
+    }
 `;
 
-export function NewBookForm(props){
+export function NewBookForm(props) {
     const { addBook, fetchBooks, authedUserId } = props;
 
     const validationSchema = yup.object().shape({
@@ -18,8 +77,8 @@ export function NewBookForm(props){
         publisher: yup.string().required("*publisher required"),
         synopsis: yup.string().test(
             "synopsis",
-            "*synopsis must be at least 30 characters long",
-            value => value && value.toString().length >= 30
+            "*synopsis must be at least 256 characters long",
+            value => value && value.toString().length >= 256
         ),
         cover_image: yup.string().url().required("*cover image must be a URL"),
         purchase_url: yup.string().url().required("*purchase url must be a URL"),
@@ -32,7 +91,7 @@ export function NewBookForm(props){
 
     return (
         <StyledNewBookForm>
-            <Formik 
+            <Formik
                 initialValues={{
                     title: "",
                     author: "",
@@ -47,29 +106,31 @@ export function NewBookForm(props){
                     <Form>
                         <div className="field">
                             <Field name="title" type="text" placeholder="Book Title" />
-                            <ErrorMessage name="title" component="div" />
+                            <ErrorMessage name="title" component="h6" />
                         </div>
                         <div className="field">
                             <Field name="author" type="text" placeholder="Author" />
-                            <ErrorMessage name="author" component="div" />
+                            <ErrorMessage name="author" component="h6" />
                         </div>
                         <div className="field">
                             <Field name="publisher" type="text" placeholder="Publisher" />
-                            <ErrorMessage name="publisher" component="div" />
+                            <ErrorMessage name="publisher" component="h6" />
                         </div>
                         <div className="field">
-                            <Field name="synopsis" type="text" placeholder="Short Synopsis" />
-                            <ErrorMessage name="synopsis" component="div" />
+                            <Field name="synopsis" type="text" component="textarea" placeholder="Short Synopsis" />
+                            <ErrorMessage name="synopsis" component="h6" />
                         </div>
                         <div className="field">
                             <Field name="cover_image" type="text" placeholder="Cover Image URL" />
-                            <ErrorMessage name="cover_image" component="div" />
+                            <ErrorMessage name="cover_image" component="h6" />
                         </div>
                         <div className="field">
                             <Field name="purchase_url" type="text" placeholder="Purchase URL" />
-                            <ErrorMessage name="purchase_url" component="div" />
+                            <ErrorMessage name="purchase_url" component="h6" />
                         </div>
-                        <button type="submit">Submit Book</button>
+                        <div className="submit-button">
+                            <button type="submit">Submit Book</button>
+                        </div>
                     </Form>
                 )}
             />
@@ -78,6 +139,6 @@ export function NewBookForm(props){
 }
 
 export default connect(
-	state => state,
-	actionCreators
+    state => state,
+    actionCreators
 )(NewBookForm)
